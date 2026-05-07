ee9b567b763272419691ad2817f7a561e021ae26
HasanMuhassin <muhassin.live@gmail.com>
2026-05-07 20:24:16 +0530

    fix final vercel setup

--- server/server.js
+++ server/server.js
@@ -21,9 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*'
 }));
 
 app.options('*', cors());
@@ -85,7 +83,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
